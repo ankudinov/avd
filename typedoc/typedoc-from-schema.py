8fcbfe8748bfbbb644110403e3ff14f407a5eaef
pa <pa@arista.com>
2025-10-20 13:31:29 +0000

    .

--- typedoc/typedoc-from-schema.py
+++ typedoc/typedoc-from-schema.py
@@ -67,11 +67,6 @@ for schema_name, schema_paths in SCHEMAS.items():
         except:
             required = False
         md_doc_list = [
-            f"/**",
-            f" * @groupDescription Events",
-            f" * Events are for...",
-            f" * @showGroups",
-            f" */",
             f"---",
             f"title: \"{key_title}\"",
             f"---",
