8fe20b20b736189e37a81f40da3c421a09038087
pa <pa@arista.com>
2025-10-21 10:32:40 +0000

    .

--- typedoc/typedoc-from-schema.py
+++ typedoc/typedoc-from-schema.py
@@ -89,6 +89,8 @@ def generate_docs_for_keys(keys_schema: AristaAvdSchema, parent_key: str = "", d
                     children_md.append(f"    - {key_name}/{subkey}.md")
                     child_keys_md.append(f"- [`{subkey}`]({key_name}/{subkey}.md)")
 
+                generate_docs_for_keys(v.keys, parent_key=key_name, doc_dir=os.path.join(doc_dir, key_name))
+
         md = MdDoc()
         md.add([
             f"---",
